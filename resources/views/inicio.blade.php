988eb7ea097f32ab270ccf711d13b4a34fd1a5e9
Andres Salazar <torneosant@gmail.com>
2026-07-03 19:49:39 -0500

    Proyecto base Laravel funcionando

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Gestión Clubes</title>
+</head>
+<body>
+
+    <h1>Gestión Clubes</h1>
+
+    <h2>Bienvenido Andrés</h2>
+
+    <p>Primer software desarrollado desde cero.</p>
+
+</body>
+</html>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gestión Clubes</title>
+</head>
+
+<body>
+
+    <header>
+        <h2>⚽ Gestión Clubes</h2>
+    </header>
+
+    <hr>
+
+    @yield('contenido')
+
+</body>
+</html>
